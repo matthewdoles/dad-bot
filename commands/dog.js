9050dc49b20711769e4716112a3a210c6e6bdb43
Donnie Waters <donniewaters29@gmail.com>
2018-07-29 23:42:56 -0400

    Add dog breed requests

--- commands/dog.js
+++ commands/dog.js
@@ -3,12 +3,21 @@ const snekfetch = require('snekfetch');
 module.exports = {
     name: 'dog',
     description: 'A random dog wow.',
-    async execute(message) {
-      snekfetch.get('https://dog.ceo/api/breeds/image/random')
+    async execute(message, args) {
+      const breed = args[0];
+      if (breed === 'list') {
+        message.channel.send('List of breeds can be found here: https://dog.ceo/dog-api/breeds-list')
+          .catch(console.log);
+        return;
+      }
+      snekfetch.get(`https://dog.ceo/api/breed/${breed}/images/random`)
         .then(({ body }) => {
           message.channel.send(body.message)
             .catch(console.log);
         })
-        .catch(console.log);
+        .catch(err => {
+          message.channel.send('UwU sowwy but no images were found for that breed UwU.')
+            .catch(console.log);
+        });
     },
 };
