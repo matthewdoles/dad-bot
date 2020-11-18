76fab46fce51173779a534c372223c7a594e950a
Matthew Doles <matteddied@gmail.com>
2020-11-17 19:54:54 -0500

    Cron job to post good morning message

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const cron = require('cron');
+const snekfetch = require('snekfetch');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -15,6 +16,46 @@ for (const file of commandFiles) {
   // key is command name; value the exported module
   client.commands.set(command.name, command);
 }
+
+const scheduledMessage = new cron.CronJob('0 9 * * *', async () => {
+  const channel = client.channels.get('135863390001299456');
+  const today = new Date();
+  const randomReply = Math.floor(Math.random() * 4);
+  const reply = [
+    'Good morning gamers! ',
+    'gmg ',
+    'Good morning gamers! Lets get this bread. ',
+    'Rise young kings, time to get that bread gamers. ',
+  ];
+
+  if (today.getDay() === 3) {
+    snekfetch
+      .get(
+        'https://api.imgur.com/3/gallery/search/time/month?q=it+is+wednesday+my+dudes'
+      )
+      .set({
+        Authorization: `Client-ID ${process.env.imgur_client_id}f00543f819732df`,
+      })
+      .then(({ body }) => {
+        return channel.send(
+          reply[randomReply] + 'It is Wednesday my dudes ' + body.data[0].link
+        );
+      })
+      .catch(console.log);
+  } else {
+    snekfetch
+      .get(
+        'https://api.imgur.com/3/gallery/search/time/week?q=good+morning&perPage=5'
+      )
+      .set({ Authorization: `Client-ID ${process.env.imgur_client_id}` })
+      .then(({ body }) => {
+        return channel.send(
+          reply[randomReply] + body.data[Math.floor(Math.random() * 50)].link
+        );
+      })
+      .catch(console.log);
+  }
+});
 
 // set status, provide help command
 client.on('ready', () => {
@@ -63,13 +104,6 @@ client.on('message', async (message) => {
       `You didn't provide any arguments, ${message.author} silly!`
     );
   }
-
-  const scheduledMessage = new cron.CronJob('* * * * *', () => {
-    const channel = client.channels.get('604097296254107680');
-    channel.send('Hi');
-  });
-
-  scheduledMessage.start();
 
   try {
     if (
