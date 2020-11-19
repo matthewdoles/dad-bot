d256d2dcf2daaf984772e8cc3587a0d07ba491f1
Matthew Doles <matteddied@gmail.com>
2020-11-18 19:47:44 -0500

    format index.js, rename command files

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
-const cron = require('cron');
-const snekfetch = require('snekfetch');
 const Discord = require('discord.js');
+const goodMorningMessage = require('./commands/good-morning');
 const client = new Discord.Client();
-
 client.commands = new Discord.Collection();
 require('dotenv').config();
 
@@ -11,68 +9,25 @@ require('dotenv').config();
 const commandFiles = fs
   .readdirSync('./commands')
   .filter((file) => file.endsWith('.js'));
-for (const file of commandFiles) {
+
+// key is command name, value the exported module
+commandFiles.forEach((file) => {
   const command = require(`./commands/${file}`);
-  // key is command name; value the exported module
   client.commands.set(command.name, command);
-}
-
-const scheduledMessage = new cron.CronJob(
-  '0 9 * * *',
-  async () => {
-    const channel = client.channels.get('135863390001299456');
-    const today = new Date();
-    const randomReply = Math.floor(Math.random() * 4);
-    const reply = [
-      'Good morning gamers! ',
-      'gmg. ',
-      'Good morning gamers! Lets get this bread. ',
-      'Rise young kings, time to get that bread gamers. ',
-    ];
-
-    if (today.getDay() === 3) {
-      snekfetch
-        .get(
-          'https://api.imgur.com/3/gallery/search/time/month?q=it+is+wednesday+my+dudes'
-        )
-        .set({
-          Authorization: `Client-ID ${process.env.imgur_client_id}`,
-        })
-        .then(({ body }) => {
-          return channel.send(
-            reply[randomReply] + 'It is Wednesday my dudes ' + body.data[0].link
-          );
-        })
-        .catch(console.log);
-    } else {
-      snekfetch
-        .get(
-          'https://api.imgur.com/3/gallery/search/time/week?q=good+morning&perPage=5'
-        )
-        .set({ Authorization: `Client-ID ${process.env.imgur_client_id}` })
-        .then(({ body }) => {
-          return channel.send(
-            reply[randomReply] + body.data[Math.floor(Math.random() * 50)].link
-          );
-        })
-        .catch(console.log);
-    }
-  },
-  null,
-  false,
-  'America/Indiana/Indianapolis'
-);
+});
 
 // set status, provide help command
 client.on('ready', () => {
-  client.user.setActivity(' out for my dads: !dadhelp', { type: 'Watching' });
+  client.user.setActivity('!dadhelp');
 });
 
 client.on('message', async (message) => {
+  // bot tagged response
   if (message.isMentioned(client.user)) {
     return message.channel.send('How can I help you son?');
   }
 
+  // daniel craig
   if (message.content.toLowerCase() === 'daniel craig') {
     return message.channel.send(
       'holy poggers guys https://imgur.com/a/jXOA1Aw'
@@ -80,8 +35,9 @@ client.on('message', async (message) => {
   }
 
   // return if message doesn't begin with command prefix or is a message from the bot
-  if (!message.content.startsWith(process.env.prefix) || message.author.bot)
+  if (!message.content.startsWith(process.env.prefix) || message.author.bot) {
     return;
+  }
 
   // split command arguments by spaces
   const args = message.content.slice(process.env.prefix.length).split(/ +/);
@@ -107,7 +63,7 @@ client.on('message', async (message) => {
   // check if any args required and entered
   if (command.args && !args.length) {
     return message.channel.send(
-      `You didn't provide any arguments, ${message.author} silly!`
+      message.author + " you didn't provide any parameters silly!"
     );
   }
 
@@ -116,7 +72,7 @@ client.on('message', async (message) => {
       commandName === 'goodmorningstart' ||
       commandName === 'goodmorningstop'
     ) {
-      command.execute(scheduledMessage);
+      command.execute(goodMorningMessage);
     }
     command.execute(message, args);
   } catch (error) {
